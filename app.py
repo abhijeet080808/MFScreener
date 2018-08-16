29ff92b15e44b48938d61f9084602676024c83de
Abhijeet Baruah <abhijeet080808@gmail.com>
2018-08-16 19:17:38 +0530

    Add flask

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# https://www.tutorialspoint.com/flask/flask_quick_guide.htm
+# http://flask.pocoo.org/docs/1.0/
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html', name = "Abhijeet")
+
+@app.route('/test')
+def test():
+  return render_template('test.html')
+
+
+if __name__ == '__main__':
+  app.run()
